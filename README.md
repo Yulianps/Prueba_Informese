84e8ab51ee81a1b5a6e173b0d02f5dc0a82c219a
Wilson Yulian Pinzón Sierra <wpinzon@igerencia.com>
2022-03-07 01:38:38 -0500

    Solucion_prueba_técnica

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Prueba_Informese
 Prueba Técnica
+
+Contiene los siguientes archivos
+
+Carpeta docs: encontramos los archivos 
+-Prueba Técnica Python
+-Prueba Teórica Python Resuelta
+
+
+Carpeta inputs: Encontramos los archivos insumos para el desarrollo de la prueba
+-ejercicio1_b1_xlsx
+-ejercicio1_b2.txt
+-ejercicio_b1.json
+
+Carpeta outputs: Encontramos las carpetas
+-ejercicio1: 
+--Archivo de texto plano outputdf.csv respuesta al ejercicio1
+-ejercicio2
+-ejercicio3
+
+Carpeta src: Encontramos las carpetas con los archivos .py correspondientes a cada ejercicio
+
+Archivo main.py
+Ejecutable de la prueba 
+Modo de uso:
+Ejecute el comando python.exe .\main.py --ejercicio=ejercicio1 
+
+Archivo requirements.txt 
+Modo de uso:
+Ejecute el comando pip install -r requirements.txt 
+
+Instala las dependencias necesarias para ejecutar la prueba 
+
+----------------------------------------------
+Version de Python 3.10.2
+
+Prueba desarrollada por:
+Wilson Yulian Pinzón Sierra
+Marzo 2022
--- src/ejercicio3/ejercicio3.py
+++ src/ejercicio3/ejercicio3.py
@@ -1,6 +1,8 @@
 import re
 import pandas as pd
 import numpy as np
+from functools import reduce
+
 
 def process_ejercicio3 ():
     
@@ -8,16 +10,15 @@ def process_ejercicio3 ():
     lst = 25*['a', 'b', 'c', 'd']
     dfrand_t = pd.DataFrame(list(zip(lst, lst,lst)),columns=list('123'))
 
-    #Creamos el resto de columnas
+    #Creamos las 7 columnas faltantes pobladas con enteros del 1 al 100
 
     dfrand_n1 = pd.DataFrame(np.random.randint(0,100,size=(100, 7)), columns=list('4567890'))
     dfrand_n2 = pd.DataFrame(np.random.randint(0,100,size=(100, 7)), columns=list('4567890'))
 
-    #Creamos el dataframe resultante
+    #Creamos los  dataframes resultantes con las condiciones requeridas
 
     dfrand_1 = pd.concat([dfrand_t,dfrand_n1], axis=1)
     dfrand_2 = pd.concat([dfrand_t,dfrand_n2], axis=1)
-
 
     print("Para DF's diferentes -->")
     comparacion = comparar(dfrand_1, dfrand_2)
@@ -27,15 +28,14 @@ def process_ejercicio3 ():
 
     return
 
+#Función comparar retorna el porcentaje de registros iguales que hay en dos dataframes comparandolos
 
 def comparar (df1, df2):    
     result = (df1 == df2)
     
     conteo = result[result == True].count()
-    suma = 0
-    for i in range(0,result.shape[1]):
-        suma = suma + conteo[i]
-    porcentaje = suma/(result.shape[0]*result.shape[1])*100
+    sumatoria = reduce(lambda a,b : a+b, conteo)
+    porcentaje = sumatoria/(result.shape[0]*result.shape[1])*100
     print("El porcentaje de registros iguales en los dos dataframes es de: ")
     print(str(porcentaje) + " %")
-    return conteo
+    return 
